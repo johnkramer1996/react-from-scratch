dd1213b073274ebae0691c8c40926c67d17a14b1
Vitalii Zinoviev <vitaliczinoviev@gmail.com>
2023-05-02 16:48:31 +0300

    Added func markUpdateTimeFromFiberToRoot

--- src/react/sheduleWork.js
+++ src/react/sheduleWork.js
@@ -14,7 +14,7 @@ import { createWorkInProgress } from './fiber'
  */
 
 export function sheduleWork(fiber) {
-  var root = getRoot(fiber)
+  var root = markUpdateTimeFromFiberToRoot(fiber)
   if (
     (executionContext & LegacyUnbatchedContext) !== NoContext &&
     (executionContext & (RenderContext | CommitContext)) === NoContext
@@ -73,19 +73,42 @@ function finishSyncRender(root) {
   commitRoot(root)
 }
 
-function getRoot(fiber) {
-  var node = fiber.return
-  var root = null
+function markUpdateTimeFromFiberToRoot(fiber, expirationTime) {
+  // Update the source fiber's expiration time
+  if (fiber.expirationTime < expirationTime) {
+    fiber.expirationTime = expirationTime
+  }
+
   var alternate = fiber.alternate
 
-  if (node === null && fiber.tag === HostRoot) root = fiber.stateNode
-  else {
+  if (alternate !== null && alternate.expirationTime < expirationTime) {
+    alternate.expirationTime = expirationTime
+  }
+
+  var node = fiber.return
+  var root = null
+
+  if (node === null && fiber.tag === HostRoot) {
+    root = fiber.stateNode
+  } else {
     while (node !== null) {
       alternate = node.alternate
+
+      if (node.childExpirationTime < expirationTime) {
+        node.childExpirationTime = expirationTime
+
+        if (alternate !== null && alternate.childExpirationTime < expirationTime) {
+          alternate.childExpirationTime = expirationTime
+        }
+      } else if (alternate !== null && alternate.childExpirationTime < expirationTime) {
+        alternate.childExpirationTime = expirationTime
+      }
+
       if (node.return === null && node.tag === HostRoot) {
         root = node.stateNode
         break
       }
+
       node = node.return
     }
   }
